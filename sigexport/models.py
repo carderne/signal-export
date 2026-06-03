ed9c4ceffa5bc16bf02e9ac36806f7fc099c7eb5
Moresteck <moresteck@protonmail.com>
2026-06-03 10:23:29 +0200

    feat: support for more media formats (#204)
    mp3, ogg, opus, wav, flac, mpeg, webm, qt, mov, mkv, webp, apng, svg

--- sigexport/models.py
+++ sigexport/models.py
@@ -99,6 +99,9 @@ def is_image(p: str) -> bool:
         "gif",
         "tif",
         "tiff",
+        "webp",
+        "apng",
+        "svg",
     ]
 
 
@@ -107,6 +110,11 @@ def is_audio(p: str) -> bool:
     return len(suffix) > 1 and suffix[-1] in [
         "m4a",
         "aac",
+        "mp3",
+        "ogg",
+        "opus",
+        "wav",
+        "flac",
     ]
 
 
@@ -114,6 +122,11 @@ def is_video(p: str) -> bool:
     suffix = p.split(".")
     return len(suffix) > 1 and suffix[-1] in [
         "mp4",
+        "mov",
+        "mkv",
+        "mpeg",
+        "webm",
+        "qt",
     ]
 
 
